9e5ef35567ac7f55c80da3c55af664861f3c2802
Tim Culverhouse <tim@timculverhouse.com>
2025-06-06 09:47:51 -0500

    assert: remove logs
    Panic provides a good enough stack trace

--- assert/assert.go
+++ assert/assert.go
@@ -2,34 +2,26 @@
 
 package assert
 
-import (
-	"log"
-)
-
 func True(condition bool) {
 	if !condition {
-		log.Output(2, "assertion failure: value is not true")
 		panic("assertion failure: value is not true")
 	}
 }
 
 func False(condition bool) {
 	if !condition {
-		log.Output(2, "assertion failure: value is not false")
 		panic("assertion failure: value is not false")
 	}
 }
 
 func NotNil(v any) {
 	if v == nil {
-		log.Output(2, "assertion failure: value is nil")
 		panic("assertion failure: value is nil")
 	}
 }
 
 func Nil(v any) {
 	if v != nil {
-		log.Output(2, "assertion failure: value is not nil")
 		panic("assertion failure: value is not nil")
 	}
 }
